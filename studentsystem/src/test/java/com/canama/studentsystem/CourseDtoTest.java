10e4b39674edd25872d22a658feed5552d75e0c0
Caner <caner@mustafa.ch>
2025-03-06 14:34:39 +0100

    Unit Tests

--- studentsystem/src/test/java/com/canama/studentsystem/CourseDtoTest.java
+++ studentsystem/src/test/java/com/canama/studentsystem/CourseDtoTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
 import java.util.Set;
 
 class CourseDtoTest {
